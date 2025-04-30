324e773f6e478d6aff7dec0253748f477d1e3c00
Quinten Woolderink <your_email@example.com>
2025-04-30 16:31:07 +0200

    Inntroduced classes

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -1,8 +1,12 @@
 package com.chess.engine.pieces;
 
 import com.chess.engine.Alliance;
+import com.chess.engine.board.Move;
+import com.chess.engine.board.Board;
 
-public class Piece {
+import java.util.List;
+
+public abstract class Piece {
 	protected final int piecePosition;
 	protected final Alliance pieceAlliance;
 
@@ -10,4 +14,6 @@ public class Piece {
 		this.pieceAlliance = pieceAlliance;
 		this.piecePosition = piecePosition;
 	}
+
+	public abstract List<Move> calculateLegalMoves(final Board board);
 }
