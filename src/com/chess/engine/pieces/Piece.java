1d8b185f38466fa0a5f4152ad683c28c0ae5cdcc
Quinten Woolderink <your_email@example.com>
2025-04-30 16:15:24 +0200

    Alliance enum made

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -1,5 +1,13 @@
 package com.chess.engine.pieces;
 
-public class Piece {
+import com.chess.engine.Alliance;
 
+public class Piece {
+	protected final int piecePosition;
+	protected final Alliance pieceAlliance;
+
+	Piece(final int piecePosition, final Alliance pieceAlliance) {
+		this.pieceAlliance = pieceAlliance;
+		this.piecePosition = piecePosition;
+	}
 }
